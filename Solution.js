ade67b5e3f87979edf4b4917cfe084ed7d7f5e78
Dipto Das <ddipto396@gmail.com>
2023-02-07 14:23:40 +0600

    Solution of problem-2 is completed

--- Solution.js
+++ Solution.js
@@ -1,6 +1,6 @@
 /* ------------
 
-Solution of problem no- 1
+Solution of PROBLEM-1
 
 Description: Below is the function called mindGame, that contains a single parameter. A number is passed
 as argument while invoking the function and sets as the value of parameter. The function multiply the number 
@@ -28,5 +28,39 @@ function mindGame(mindGame_number){
 // console.log(mindGame(5));
 // console.log(mindGame(30));
 // console.log(mindGame(33));
+
+
+
+
+/* ------------
+
+Solution of the PROBLEM-2
+
+Description: Below is the function called evenOdd, that contains a single parameter. A string is passed
+as argument while invoking the function and sets as the value of parameter. The function counts total character
+consists into string. Finally, the function determines even or odd based on total characters of string. 
+
+---------------*/ 
+
+function evenOdd(word){
+
+    if(typeof word !== 'string'){
+        return 'ERROR! Please provide a valid string';
+    }
+
+    else{
+        let word_length = word.length;
+            if(word_length % 2 == 0){
+                return 'even';
+            }
+            else{
+                return 'odd';
+            }
+    }
+}
+
+// console.log(evenOdd('Phero'));
+// console.log(evenOdd('Batch7'));
+// console.log(evenOdd('chatgpt'));
 
 
--- problem-2.js
+++ problem-2.js
@@ -1,14 +1,27 @@
 /* ------------
-Answer to the problem no- 2
+
+Solution of the PROBLEM-2
+
+Description: Below is the function called evenOdd, that contains a single parameter. A string is passed
+as argument while invoking the function and sets as the value of parameter. The function counts total character
+consists into string. Finally, the function determines even or odd based on total characters of string. 
+
 ---------------*/ 
 
 function evenOdd(word){
-    let word_length = word.length;
-    if(word_length % 2 == 0){
-        return 'even';
+
+    if(typeof word !== 'string'){
+        return 'ERROR! Please provide a valid string';
     }
+
     else{
-        return 'odd';
+        let word_length = word.length;
+            if(word_length % 2 == 0){
+                return 'even';
+            }
+            else{
+                return 'odd';
+            }
     }
 }
 
