26c915065c87c03b3990cccbf5635b78e55f160d
Dipto Das <ddipto396@gmail.com>
2023-02-07 14:51:16 +0600

    Solution of problem-3 completed

--- Solution.js
+++ Solution.js
@@ -64,3 +64,41 @@ function evenOdd(word){
 // console.log(evenOdd('chatgpt'));
 
 
+
+
+/* ------------
+
+Solution of PROBLEM-3
+
+Description: Below is the function called isLGSeven, that contains a single parameter. A number is passed
+as argument while invoking the function and sets as the value of parameter. The function substract 7 from 
+the number. The function returns subtract_value if it is less than 7, otherwise it multiplies 2 with parameter
+and returns the result. 
+
+---------------*/
+
+function isLGSeven(isLGSeven_number){
+    if(typeof isLGSeven_number === 'number'){
+        let final_value = 0;
+        let difference = isLGSeven_number - 7;
+
+        if (difference < 7){
+            final_value = Number(difference);
+            return final_value;
+        }
+        else{
+            final_value = Number(isLGSeven_number * 2);
+            return final_value;
+        }
+    }
+
+    else{
+        return 'ERROR! Please provide a valid number';
+    }
+
+}
+
+// console.log(isLGSeven(6));
+// console.log(isLGSeven(-15));
+// console.log(isLGSeven(15));
+
--- problem-3.js
+++ problem-3.js
@@ -1,18 +1,33 @@
 /* ------------
-Answer to the problem no- 3
+
+Solution of PROBLEM-3
+
+Description: Below is the function called isLGSeven, that contains a single parameter. A number is passed
+as argument while invoking the function and sets as the value of parameter. The function substract 7 from 
+the number. The function returns subtract_value if it is less than 7, otherwise it multiplies 2 with parameter
+and returns the result. 
+
 ---------------*/
 
-function isLGSeven(num){
-    let final_value = 0;
-    let difference = num - 7;
-    if (difference < 7){
-        final_value = Number(difference);
-        return final_value;
+function isLGSeven(isLGSeven_number){
+    if(typeof isLGSeven_number === 'number'){
+        let final_value = 0;
+        let difference = isLGSeven_number - 7;
+
+        if (difference < 7){
+            final_value = Number(difference);
+            return final_value;
+        }
+        else{
+            final_value = Number(isLGSeven_number * 2);
+            return final_value;
+        }
     }
+
     else{
-        final_value = Number(num * 2);
-        return final_value;
+        return 'ERROR! Please provide a valid number';
     }
+
 }
 
 console.log(isLGSeven(6));
